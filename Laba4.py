a14c5235e842ab87b3e2ce816c80cda8c8f613f5
EugeneFoxWox <98107337+EugeneFoxWox@users.noreply.github.com>
2022-01-28 17:18:15 +0300

    New

---
+++ Laba4.py
@@ -0,0 +1,25 @@
+def putyg():
+  #бесконечная ульта
+    while True:
+        way = str(input('Введите путь к файлу -> '))
+        if path.exists(way) == True:
+            break
+        print('Такого пути не существует!')
+    return way
+
+
+def slovar(way):
+    pass
+
+
+
+def dublim():
+    pass
+
+
+def wywod():
+    pass
+
+  
+putyg()
+
